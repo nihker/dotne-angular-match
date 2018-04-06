c3211f7a132d7dfbd928d34f7ad478b98c92f85a
nihker <nihadkeric@gmail.com>
2018-04-06 22:39:48 +0200

    Add sass on angular app

--- DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp.SPA/src/app/register/register.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
